15aa97aa4995355dd52e692db5b4f99585ce3ecc
juma-classic <1elvisjuma@gmail.com>
2026-02-26 18:59:08 +0300

    chore: update OAuth app ID from 125428 to 129365

--- src/services/deriv-api-initializer.service.ts
+++ src/services/deriv-api-initializer.service.ts
@@ -25,7 +25,7 @@ class DerivAPIInitializer {
     private connectionListeners: Set<(connected: boolean) => void> = new Set();
 
     private readonly config: DerivAPIConfig = {
-        appId: 125428,
+        appId: 129365,
         endpoint: 'wss://ws.derivws.com/websockets/v3',
         language: 'en',
         brand: 'novaprime',
